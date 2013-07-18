1e98155b50a71f0d2c819f1dfe25b85cd8c5357c
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-18 13:52:46 -0400

    First Comment ,starting to enter givens

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -1,2 +1,9 @@
-alert("Hi");
+//Healy_Connor_Expressions_Industry
+
+//I work in the automotive industry and I can never convert KPH to MPH. Thought I would write a program that could so I can stop Googling it.
+
+var mph = ;
+
+var khp = ;
+
 
