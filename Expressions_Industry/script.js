3499f9f28ef79c0a7ef050530f433c212343bfd0
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-18 14:07:30 -0400

    figured it out

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -1,13 +1,18 @@
 //Healy_Connor_Expressions_Industry
 
-//I work in the automotive industry and I can never convert KPH to MPH. Thought I would write a program that could so I can stop Googling it.
+//I work in the automotive industry and I can never convert KPH to MPH wline watch these guys in Europe ride their motorcycle
+// at 300KPH. Thought I would write a program that could so I can stop Googling it.
 
-var mph = ;
+//var mph = ;
 
 var kph = prompt;
 
 var convert = 0.621371;
 
-var total = (kph * convert);
 
-kph = prompt("Enter the Kilometers Per Hour you are trying to convert to Miles Per Hour");
+
+total = prompt("Enter the Kilometers Per Hour you are trying to convert to Miles Per Hour") * convert;
+
+alert(total);
+
+console.log(total);
