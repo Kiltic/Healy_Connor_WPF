281da764d1e272dbdc14ecda4b63427046ea5533
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-18 18:34:51 -0400

    Var's in place

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -1,1 +1,14 @@
 //Healy_Connor_Expressions_Industry
+
+
+//Here I will calculate the total bill for my Auto Repair shop called Healy Automotive Service using
+//Parts, Labor and 7% sales tax.
+
+var parts = ;
+
+var labor = ;
+
+var tax = ;
+
+alert("Healy Automotive Service Bill Calculator");
+
