fb8584fbcf3d9c5e377577a4def6a11e15a85c0e
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-18 18:37:40 -0400

    Labor and Parts prompts in place

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -4,11 +4,17 @@
 //Here I will calculate the total bill for my Auto Repair shop called Healy Automotive Service using
 //Parts, Labor and 7% sales tax.
 
-var parts = ;
+var parts;
 
-var labor = ;
+var labor;
 
-var tax = ;
+var tax;
 
 alert("Healy Automotive Service Bill Calculator");
+
+parts = prompt("Please insert total Parts amount here. When done, Click 'OK'.");
+
+labor = prompt("Please insert total Labor amount here. When done, Click 'OK'.");
+
+
 
