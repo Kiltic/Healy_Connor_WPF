d36ce197f1b90f7672be24be31131d5c80ae3f07
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-18 18:48:00 -0400

    took away ()

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -8,15 +8,18 @@ var parts;
 
 var labor;
 
-var tax = .07;
+var total;
 
-alert("Healy Automotive Service Bill Calculator");
+
+var tax = 1.07;
+
+alert("Welcome to Healy Automotive Service's Bill Calculator.");
 
 parts = prompt("Please insert total Parts amount here. When done, Click 'OK'.");
 
 labor = prompt("Please insert total Labor amount here. When done, Click 'OK'.");
 
-total = (labor + parts) * tax;
+total = labor + parts * tax;
 
 action(total);
 
