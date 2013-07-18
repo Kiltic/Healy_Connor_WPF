1527eb2425e674eabb79831ce8a04426472355ab
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-17 22:59:40 -0400

    Dog_Years

--- Associating JS/AssociatingJS/js/Expressions_worksheet/js.js
+++ Associating JS/AssociatingJS/js/Expressions_worksheet/js.js
@@ -1,7 +1,16 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Connor
- * Date: 7/17/13
- * Time: 10:25 PM
- * To change this template use File | Settings | File Templates.
- */
+//Expressions_Worksheet_Dog_Years
+//I need to figure out Sparky's age in dog years
+var sparkyAge = 3; //Enter Sparky's age here
+
+var dogYears = 7; //DO NOT CHANGE this is the multiplier needed to calculate age in human years
+
+var total = sparkyAge * dogYears;
+var humanAge = "This is sparky's age in human years: ";
+var actualAge = "this is sparky's age in dog years: ";
+
+console.log(actualAge + sparkyAge);
+console.log(humanAge + total)
+
+
+
+//took me 13 attempts to figure out I heeded to link the index.htm to the js file.
