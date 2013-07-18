fc20f95261e559631f9cf8237012fe5444924878
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-17 23:32:57 -0400

    AverageShopping Bill
    Not finished. Need to spend time on Expressions Assignment. Be back
    soon!

--- Associating JS/AssociatingJS/js/Expressions_worksheet/js.js
+++ Associating JS/AssociatingJS/js/Expressions_worksheet/js.js
@@ -1,26 +1,7 @@
-//Expressions_Worksheet_Slice_of_Pie_Part_II
+//Expressions_Worksheet_Average_Shopping_Bill
 
-//Here we will find out how many slices of pizza each partygoer had if everybody had the same amount of slices/
+//Need to find the average spend for each week on groceries for the
+//last 5 weeks and the total spent on groceries for the same 5 weeks.
 
-var slicesPerPie = 8;     //total slices of pizza on each pie
+var grocryBills [234,124,156,300,321] //this is the Array of the Grocery Bills for the last 5 weeks
 
-var partyPeeps = 13;      //total amount of people at the party
-
-var numOfPies = 10;       //total amount of pies
-//take the number of pies times the number of slices on each pie and divide it by the number of people at the party
-
-var totalSlices = numOfPies * slicesPerPie / partyPeeps;
-
-var modulo = numOfPies * slicesPerPie % partyPeeps;
-
-var text1 = "Each partygoer had ";        //Added text here to make the answer make more sense to the reader
-
-var text2 = " slices of pie...EXACTLY!"
-
-var text3 = "Sparky gets ";
-
-var text4 = " slices too!"
-
-console.log(text1 + totalSlices + text2); //
-
-console.log(text3 + modulo +text4);
