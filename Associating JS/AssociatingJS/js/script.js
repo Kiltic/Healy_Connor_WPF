7e38d4526cac3483978206f175564a34fcf13c56
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-17 22:06:30 -0400

    Order of Operations

--- Associating JS/AssociatingJS/js/script.js
+++ Associating JS/AssociatingJS/js/script.js
@@ -1,8 +1,18 @@
-// Expressions - Order of Operations
+// Expressions - PEMDAS - Order of Operations
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+//the average is the sum of the items divided by the number of items
+var average = (quiz1 + quiz2 + quiz3 + quiz4)/4;
+//console.log(average);
+//this is cool!
 
 
-
-
-
-
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;  //no need for parenthesys. var perimeter = (length * 2) + (width * 2);Mult. happens first naturally.
+console.log(perimeter);
 
