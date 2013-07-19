b39cbd33ea16ae949971de44c6cf498f90cb364f
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-18 23:23:44 -0400

    Array Comments

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,23 +8,23 @@ var ad = [17];     //this is my array
 
 
 
-ad[0] = prompt("Please enter a plural noun below.");
-ad[1] = prompt("Please enter an adjective below");
-ad[2] = prompt("Please enter a noun below");
-ad[3] = prompt("Please enter a plural noun below.");
-ad[4] = prompt("Please enter a different plural noun below.");
-ad[5] = prompt("Please enter your name below.");
-ad[6] = prompt("Please enter a friends name below.");
-ad[7] = prompt("Please enter a name of a school below.");
-ad[8] = prompt("Please enter an adjective below.");
-ad[9] = prompt("Please enter a different adjective below.");
-ad[10] = prompt("Please enter a local pub below.");
-ad[11] = prompt("Please enter a noun below.");
-ad[12] = prompt("Please enter another noun below.");
-ad[13] = prompt("Please enter yet another noun below.");
-ad[14] = prompt("Please enter a noun below.");
-ad[15] = prompt("Please enter a verb below. Almost there.");
-ad[16] = prompt("Please enter a noun below. Then you are DONE!");
+ad[0] = prompt("Please enter a plural noun below.");         //this is the first item in my array using the index number in my array making it equal to the prompt given.
+ad[1] = prompt("Please enter an adjective below");        //this is the second item in my array using the index number in my array making it equal to the prompt given.
+ad[2] = prompt("Please enter a noun below");                 //this is the third item in my array using the index number in my array making it equal to the prompt given.
+ad[3] = prompt("Please enter a plural noun below.");             //this is the fourth item in my array using the index number in my array making it equal to the prompt given.
+ad[4] = prompt("Please enter a different plural noun below.");    //this is the fifth item in my array using the index number in my array making it equal to the prompt given.
+ad[5] = prompt("Please enter your name below.");              //this is the sixth item in my array using the index number in my array making it equal to the prompt given.
+ad[6] = prompt("Please enter a friends name below.");         //this is the seventh item in my array using the index number in my array making it equal to the prompt given.
+ad[7] = prompt("Please enter a name of a school below.");       //this is the eighth item in my array using the index number in my array making it equal to the prompt given.
+ad[8] = prompt("Please enter an adjective below.");            //this is the ninth item in my array using the index number in my array making it equal to the prompt given.
+ad[9] = prompt("Please enter a different adjective below.");     //this is the tenth item in my array using the index number in my array making it equal to the prompt given.
+ad[10] = prompt("Please enter a local pub below.");           //this is the eleventh item in my array using the index number in my array making it equal to the prompt given.
+ad[11] = prompt("Please enter a noun below.");                 //this is the twelvth item in my array using the index number in my array making it equal to the prompt given.
+ad[12] = prompt("Please enter another noun below.");        //this is the thirteenth item in my array using the index number in my array making it equal to the prompt given.
+ad[13] = prompt("Please enter yet another noun below.");      //this is the fourteenth item in my array using the index number in my array making it equal to the prompt given.
+ad[14] = prompt("Please enter a noun below.");                  //this is the fifteenth item in my array using the index number in my array making it equal to the prompt given.
+ad[15] = prompt("Please enter a verb below. Almost there.");      //this is the sixteenth item in my array using the index number in my array making it equal to the prompt given.
+ad[16] = prompt("Please enter a noun below. Then you are DONE!");       //this is the seventeenth item in my array using the index number in my array making it equal to the prompt given.
 alert("Now sit back and enjoy your work. Col. Jessep from A few Good men will be proud!");
 
 alert("Col. Jessep: You want " + ad[0] + "? Kaffee:I think I'm " + ad[1] + ". Col. Jessep: You want " +ad[0] + "? Kaffee: I want the " + ad[2] + "! Col. Jessep: You cant handle the " +ad[2] + "! \nSon we live in a world that has " + ad[3] + " and those " + ad[3] + " have to be guarded by men with " + ad[4] + ". Whos gonna do it? You? You, " + ad[5] + "? I have a greater responsibility than you can possibly fathom.  You weep for " + ad[6] + " and you curse " + ad[7] + ". You have that luxury. You have the luxury of not knowing what I know. That " + ad[6] + "'s death, while tragic, probably saved lives. And my very existince while " + ad[8] + " and " + ad[9] + " to you, saves lives You don't want the truth because deep down in places you dont talk about at " + ad[10] + ", you want me on that " + ad[11] + ". You need me on that " + ad[11]);
