20bd71923d738d5f712b52d05e6bd2cb7cf0f368
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-18 21:42:47 -0400

    added in all of the prompts

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -2,14 +2,6 @@
 
 
 
-
-
-
-
-
-
-
-
 alert("Hey now! It is Ad-Lib time!!!\nYou all know the drill. Follow the instructions and enter words into the boxes provided, at the end you will be able to read your story");
 
 var ad=[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p];
@@ -29,28 +21,27 @@ ad[10] = prompt("Please enter a local pub below.");
 ad[11] = prompt("Please enter a noun below.");
 ad[12] = prompt("Please enter another noun below.");
 ad[13] = prompt("Please enter yet another noun below.");
-ad[13] = prompt("Please enter just one more noun below.");
+ad[13] = prompt("Please enter a noun below.");
+ad[14] = prompt("Please enter just one more noun below.");
+ad[15] = prompt("Please enter a verb below. Almost there.");
+ad[16] = prompt("Please enter a noun below. Then you are DONE!");
+alert("Now sit back and enjoy your work. Col. Jessep from A few Good men will be proud!");
 
 
 
-
-
-
-
-
-//Col. Jessep:You want answers?
-//    Kaffee: I think I'm entitled to.
-//Col. Jessep: *You want answers?*
-//    Kaffee: *I want the truth!*
-//Col. Jessep: *You can't handle the truth!*
-//    [pauses]
-//Col. Jessep: Son, we live in a world that has walls, and those walls have to be guarded by men with guns.
-//    Who's gonna do it? You? You, Lt. Weinburg? I have a greater responsibility than you could possibly fathom. ' +
-//'You weep for Santiago, and you curse the Marines. You have that luxury. You have the luxury of not knowing what I know. ' +
-//'That Santiago's death, while tragic, probably saved lives. And my existence, while grotesque and incomprehensible to you, saves lives.
-//    You don't want the truth because deep down in places you don't talk about at parties, you want me on that wall, you need me on that wall.
-//    We use words like honor, code, loyalty. We use these words as the backbone of a life spent defending something. You use them as a punchline.
-//    I have neither the time nor the inclination to explain myself to a man who rises and sleeps under the blanket
-//of the very freedom that I provide, and then questions the manner in which I provide it.
-//    I would rather you just said thank you, and went on your way, Otherwise,
-//    I suggest you pick up a weapon, and stand a post. Either way, I don't give a damn what you think you are entitled to.
+alert("Col. Jessep:You want answers?
+    Kaffee: I think I'm entitled to.
+Col. Jessep: *You want answers?*
+    Kaffee: *I want the truth!*
+Col. Jessep: *You can't handle the truth!*
+    [pauses]
+Col. Jessep: Son, we live in a world that has walls, and those walls have to be guarded by men with guns.
+    Who's gonna do it? You? You, Lt. Weinburg? I have a greater responsibility than you could possibly fathom. ' +
+'You weep for Santiago, and you curse the Marines. You have that luxury. You have the luxury of not knowing what I know. ' +
+'That Santiago's death, while tragic, probably saved lives. And my existence, while grotesque and incomprehensible to you, saves lives.
+    You don't want the truth because deep down in places you don't talk about at parties, you want me on that wall, you need me on that wall.
+    We use words like honor, code, loyalty. We use these words as the backbone of a life spent defending something. You use them as a punchline.
+    I have neither the time nor the inclination to explain myself to a man who rises and sleeps under the blanket
+of the very freedom that I provide, and then questions the manner in which I provide it.
+    I would rather you just said thank you, and went on your way, Otherwise,
+    I suggest you pick up a weapon, and stand a post. Either way, I don't give a damn what you think you are entitled to!");
