72ef5313a23fd07d13c5671acdf36b6064123927
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-18 22:18:43 -0400

    different approach to the array

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,7 +4,8 @@
 
 alert("Hey now! It is Ad-Lib time!!!\nYou all know the drill. Follow the instructions and enter words into the boxes provided, at the end you will be able to read your story");
 
-var ad=[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p];
+var ad = (18);
+
 
 
 ad[0] = prompt("Please enter a plural noun below.");
@@ -21,15 +22,11 @@ ad[10] = prompt("Please enter a local pub below.");
 ad[11] = prompt("Please enter a noun below.");
 ad[12] = prompt("Please enter another noun below.");
 ad[13] = prompt("Please enter yet another noun below.");
-ad[13] = prompt("Please enter a noun below.");
-ad[14] = prompt("Please enter just one more noun below.");
-ad[15] = prompt("Please enter a verb below. Almost there.");
-ad[16] = prompt("Please enter a noun below. Then you are DONE!");
+ad[14] = prompt("Please enter a noun below.");
+ad[15] = prompt("Please enter just one more noun below.");
+ad[16] = prompt("Please enter a verb below. Almost there.");
+ad[17] = prompt("Please enter a noun below. Then you are DONE!");
 alert("Now sit back and enjoy your work. Col. Jessep from A few Good men will be proud!");
 
+alert(ad[0] + ad[1] + ad[2] + ad[3] + ad[4] + ad[5] + ad[6] + ad[7] + ad[8] + ad[9] + ad[10] + ad[11] + ad[12] + ad[13] + ad[14] + ad[15] + ad[16] + ad[17]);
 
-
-alert("Col. Jessep:You want answers? Kaffee: I think I'm entitled to. Col. Jessep: You want answers? Kaffee: I want the truth! Col. Jessep: You can't handle the truth! Col. Jessep: Son, we live in a world that has walls, and those walls have to be guarded by men with guns.
-Who's gonna do it? You? You, Lt. Weinburg? I have a greater responsibility than you could possibly fathom. '
-'You weep for Santiago, and you curse the Marines. You have that luxury. You have the luxury of not knowing what I know. '
-'That Santiagos death, while tragic, probably saved lives. And my existence, while grotesque and incomprehensible to you, saves lives.You dont want the truth because deep down in places you dont talk about at parties, you want me on that wall, you need me on that wall.We use words like honor, code, loyalty. We use these words as the backbone of a life spent defending something. You use them as a punchline.I have neither the time nor the inclination to explain myself to a man who rises and sleeps under the blanket of the very freedom that I provide, and then questions the manner in which I provide it. I would rather you just said thank you, and went on your way, Otherwise, I suggest you pick up a weapon, and stand a post. Either way, I dont give a damn what you think you are entitled to!");
