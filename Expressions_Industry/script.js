81e7687e662a8273ff8bf42f899c48b32728c88a
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-18 20:03:16 -0400

    finally figures it out
    Had to put a + sign in front of the prompt inputs to add them together.

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -4,29 +4,25 @@
 //Here I will calculate the total bill for my Auto Repair shop called Healy Automotive Service using
 //Parts, Labor and 7% sales tax.
 
-var parts;
-
-var labor;
+var subtotal;
 
 var total;
-
-var subtotal;
 
 var tax = 1.07;
 
 alert("Welcome to Healy Automotive Service's Bill Calculator.");
-
-parts = prompt("Please insert total Parts amount here. When done, Click 'OK'.");
-
-labor = prompt("Please insert total Labor amount here. When done, Click 'OK'.");
-
-subtotal = (parts + labor);
+console.log("Welcome to Healy Automotive Service's Bill Calculator.");
+var parts = prompt("Please insert total Parts amount here. When done, Click 'OK'.");
+console.log("The parts come to: $" + parts);
+var labor = prompt("Please insert total Labor amount here. When done, Click 'OK'.");
+console.log("The Labor comes to: $" + labor);
+subtotal = +parts + +labor;
 
 total = subtotal * tax;
 
-alert(total);
+alert("the subtotal of the bill is $" + subtotal + " and the total with tax comes to $" + total);
 
-console.log(total);
+console.log("the subtotal of the bill is $" + subtotal + " and the total with tax comes to $" + total);
 
 
 
