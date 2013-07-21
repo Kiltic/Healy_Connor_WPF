da884dc9bf97127b2793ce081f54680db2476d04
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-20 22:29:58 -0400

    Rude Island Amusement Park.
    In inches, can you ride the ride!?

--- Week_3_Practice/script.js
+++ Week_3_Practice/script.js
@@ -1,16 +1,32 @@
-//Conditional Logic
+//Conditional Logic -else if
 
-var kidHeight = 48;
+var kidHeight;
 var minHeight = 48;
+var wParentHeight = 45;
 
-//if the child is old enough, print to alert, "You can ride"
-//if the kid is over 48" in height
+
+alert("Hey kids! Welcome to Rude Island Amusement Park!");
+
+kidHeight = prompt("How tall are you in inches?");
+
 
 if(kidHeight >= minHeight){
-    //code performed if condition is true
-    alert("You are tall enough, ride that bitch!")
-}else{
-    //code performed if condition is false
 
-    alert("Sorry bro, You are screwed!")
+    //You can ride
+
+    alert("You are tall enough, ride that bitch!");
+    alert("Thank you for riding our bitchin' ride! Come again!")
+}else if(kidHeight > wParentHeight){
+
+    //you need a parent
+    alert("Haha, you need your mommy!  Go get her fine ass and come back!");
+    alert("Thank you for riding our bitchin' ride! Come again!")
+}else{
+
+     //Grow up!
+
+    alert("Sorry bro, You are screwed!");
+    alert("Go home! Next time read the rules on the site before you make your mommy drive you all of the way here!!")
 }
+
+
