174a774bcd7dda71839dcf22c5b1b4d127c1a92f
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-20 22:03:03 -0400

    Doing some practice

---
+++ Week_3_Practice/script.js
@@ -0,0 +1,16 @@
+//Conditional Logic
+
+var kidHeight = 48;
+var minHeight = 48;
+
+//if the child is old enough, print to alert, "You can ride"
+//if the kid is over 48" in height
+
+if(kidHeight >= minHeight){
+    //code performed if condition is true
+    alert("You are tall enough, ride that bitch!")
+}else{
+    //code performed if condition is false
+
+    alert("Sorry bro, You are screwed!")
+}
