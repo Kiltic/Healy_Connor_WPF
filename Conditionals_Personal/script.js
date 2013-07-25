adf89f2a4db6fe637c0e0e2c82670891ba9ca4c1
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-24 21:56:22 -0400

    hmmmm try the ===

--- Conditionals_Personal/script.js
+++ Conditionals_Personal/script.js
@@ -13,11 +13,11 @@ alert("I will tell what video game you should play based on your mood. Realy? 'C
 
 answer = prompt("what mood are you in, happy, angry or mischievous?");
 
-if (answer = "happy"){
+if (answer === "happy"){
     alert("Oh, you have to play Sonic: Ultimate Racing")
-}else if(answer = "angry"){
+}else if(answer === "angry"){
     alert("Gears of War 3, no doubt, Gears of war 3. Blood, death, AWESOMENESS, murder and mayhem!!  Nothing better, you will get up sweating.");
-}else if(answer = "mischievous"){
+}else if(answer === "mischievous"){
     alert("No doubt, Assasin's Creed 3. Steal from old ladies, slit throats and keep walking, drink beer and cause all sorts of ruckus. Too much fun!");
 
 };
