5dbb6507fc038128ae53210b5457a41f1180caf3
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-24 21:13:37 -0400

    try again

--- Conditionals_Personal/script.js
+++ Conditionals_Personal/script.js
@@ -8,7 +8,7 @@ var movie = 2;
 var photoshop = 3;
 var time;
 var pnr;
-var answer [xbox, movie, photoshop];
+//var answer ["xbox", "movie", "photoshop"];
 
 alert("Welcome to Connor's 'What do you have time to do?' calculator");
 alert("It is night-time, what do you have time to do when you are done with your homework?");
