f0b4879787b283baed53c585978196cb5b163e57
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-24 21:23:34 -0400

    testing if /else

--- Conditionals_Personal/script.js
+++ Conditionals_Personal/script.js
@@ -1,2 +1,16 @@
 //Connor_Healy Conditionals_Personal
 
+//Based on mood, what kind of video game do you want to play?
+
+var fun;
+var angry;
+var boyorgirl;
+
+boyorgirl = prompt("type boy or girl.");
+
+if boyorgirl = "boy"{
+    alert("WOW you are a boy");
+
+}else{
+    alert("you are a girl, HAHAHA!!");
+}                                     ;
