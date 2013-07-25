4df4035eecf686b6127603e39ed2b2796e403241
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-25 18:00:56 -0400

    more word changes

--- Conditionals_Industry/script.js
+++ Conditionals_Industry/script.js
@@ -12,7 +12,7 @@ workHard = prompt("Question 1 of 3. Are you willing to work endlessly and tirele
 
 caffine = prompt("Great! Question 2 of 3. Are you addicted or do you have no problem becoming addicted to a caffinated beverage, ie: RedBull, Monster Energy Drinks or Soda? Enter yes or no and 'Click OK'");
 
-space = prompt("Awesome! Question 3 of 3. Do you have no problems being in a confined space, like a basement or a cubicle, with no windows or fresh air for days on end? Enter yes or no then 'Click OK'");
+space = prompt("Awesome! Question 3 of 3. Are you fine with being in a confined space, like a basement or a cubicle, with no windows or fresh air for days on end? Enter yes or no then 'Click OK'");
 
 if (workHard, caffine === "yes" && space === "yes" ){
     alert("Lucky you, you are ready to be a Wed Designer, and will probably die early of a heart attack!");
