929a32b96843a4a1df76ba3f8e4cd48682b6c163
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-24 22:08:33 -0400

    I will put that on hold

--- Conditionals_Personal/script.js
+++ Conditionals_Personal/script.js
@@ -20,9 +20,9 @@ if (answer === "happy"){
 }else if(answer === "mischievous"){
     alert("No doubt, Assasin's Creed 3. Steal from old ladies, slit throats and keep walking, drink beer and cause all sorts of ruckus. Too much fun!");
 
-}else if(answer = ""){
-    alert("What are you trying to do, I am new at this! wait, you can't spell mischievous, can you.  Try again");
+};//else if(answer = ""){
+    //alert("What are you trying to do, I am new at this! wait, you can't spell mischievous, can you.  Try again");
 
-};
+//};
 
 
