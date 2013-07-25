e5a2ac8e052a931153cbbbdaa05a0939862a6bc8
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-25 11:49:40 -0400

    Trying it out

--- Conditionals_Industry/script.js
+++ Conditionals_Industry/script.js
@@ -1,2 +1,25 @@
-alert("hi");
+//Connor_Healy_Conditionals_Industry
+
+//This will prove if you are ready to become a Web Designer.
+
+
+var workHard;
+var caffine;
+var space;
+
+alert("Welcome to Connor's 'Are you really ready to be a Web Designer?' quiz. Let's see if you are. 'Click OK'");
+workHard = prompt("Question 1 of 3. Are you willing to work endlessly and tirelessly for days straight without any recognition for your work or what you have done? Enter yes or no below then 'Click OK'.");
+
+caffine = prompt("Great! Question 2 of 3. Are you addicted or do you have no problem becoming addicted to a caffinated beverage, ie: RedBull, Monster or soda? Enter yes or no and 'Click OK'");
+
+space = prompt("Awesome! Question 3 of 3. Do you have any problems being in a confined space, like a basement or a cubicle, with no windows or fresh air for days on end? Enter yes or no then 'Click OK'");
+
+if (workHard, caffine === "yes" && space === "yes" ){
+    alert("Lucky you, you are ready to be a Wed Designer, and will probably die early of a heart attack!");
+
+}else{
+    alert("I am sorry to say you do NOT have what it takes to be a Wed Designer, you are now doomed to a long, happy life");
+
+}
+
 
