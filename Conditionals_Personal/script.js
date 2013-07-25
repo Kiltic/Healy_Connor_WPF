a6df5c71a70344bd8a0c4e7dc7e7647d174965cf
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-24 21:10:02 -0400

    trying to sort it out

--- Conditionals_Personal/script.js
+++ Conditionals_Personal/script.js
@@ -1,2 +1,18 @@
 //Connor_Healy Conditionals_Personal
 
+//Here I will calculate, depending on what time it is, what I have time to do
+//at night before I get in trouble with my wife for staying up too late.
+
+var xbox = 4;
+var movie = 2;
+var photoshop = 3;
+var time;
+var pnr;
+var answer [xbox, movie, photoshop];
+
+alert("Welcome to Connor's 'What do you have time to do?' calculator");
+alert("It is night-time, what do you have time to do when you are done with your homework?");
+time = prompt("Please enter the current time.");
+answer = prompt("Please choose one of these three choices of what you want to do before you go to bed. you can write, xbox, movie or Photoshop");
+
+
