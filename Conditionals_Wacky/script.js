7b293c2fe6787b907e83f3dac14ec7296c06ca1c
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-25 22:38:37 -0400

    drool fix

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -10,8 +10,7 @@ var fannyPack;
 alert("Welcome to Connor's Chick-Checker!");
 alert("Here we will find out, according to me, whether you have what it takes to get chicks! Ready? 'Click OK'");
 
-drool = prompt("on a scale of 1-10, 1 being never and 10 being like a water fountain, how much would you say " +
-    "that you drool while talking to chicks?");
+drool = prompt("Are you a drooler? Would you say that you drool ALOT while talking to chicks?");
 
 bieber= prompt("How many Justin Bieber tee shirts do you have hanging in your closet?");
 
