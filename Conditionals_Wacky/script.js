2bad007c28ca146fa5fb11140a912f5487c77721
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-25 22:19:04 -0400

    trying something new

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -4,3 +4,18 @@ Connor_Healy_Conditionals_Wacky
 
 var drool;
 var bieber;
+var
+
+alert("Welcome to Connor's Chick-Checker!");
+alert("Here we will find out, according to me, whether you have what it takes to get chicks! Ready? 'Click OK'");
+
+drool = prompt("on a scale of 1-10, 1 being never and 10 being like a water fountain, how much would you say " +
+    "that you drool while talking to chicks?");
+
+bieber=("How many Justin Bieber tee shirts do you have hanging in your closet?");
+
+if(bieber != 0){
+    alert("Get out, you are no longer worthy to use the Chick-Checker!! I mean it, GO AWAY!");
+}else{
+    prompt("")
+}
