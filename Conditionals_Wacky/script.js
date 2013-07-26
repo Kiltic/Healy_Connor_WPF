f60ea41a3244ab7ee63eda3c57d3ec65f83278ba
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-25 22:23:35 -0400

    commented out the title

--- Conditionals_Wacky/script.js
+++ Conditionals_Wacky/script.js
@@ -1,15 +1,15 @@
-Connor_Healy_Conditionals_Wacky
+//Connor_Healy_Conditionals_Wacky
 
 //Here i will take a few factors and decide whether you can get chicks easily or not.
 
-//var drool;
-//var bieber;
+var drool;
+var bieber;
 
 
 alert("Welcome to Connor's Chick-Checker!");
 alert("Here we will find out, according to me, whether you have what it takes to get chicks! Ready? 'Click OK'");
 
-/*drool = prompt("on a scale of 1-10, 1 being never and 10 being like a water fountain, how much would you say " +
+drool = prompt("on a scale of 1-10, 1 being never and 10 being like a water fountain, how much would you say " +
     "that you drool while talking to chicks?");
 
 bieber= prompt("How many Justin Bieber tee shirts do you have hanging in your closet?");
