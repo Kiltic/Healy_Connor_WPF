66794a5a519034aa971a27ee9ef3b6fc126398c0
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-08-01 12:21:35 -0400

    got it to work, but I cant make it "ADD" all of the ages together, it just prints them out one after the next.

--- Functions_Personal/script.js
+++ Functions_Personal/script.js
@@ -2,33 +2,33 @@
 
 //This is the family Age Calculator.
 
+
+
+
 alert("This calculator will add up the ages of everyone in your immediate family. " +
     "Enter the ages below. If it doesn't apply leave it blank. 'Click OK when ready.'");
 
-var age1 = prompt("How old are you?");
-var age2 = prompt("How old is your wife/Girlfriend/Husband/Boyfriend?");
-var age3 = prompt("How old is your first born?");
-var age4 = prompt("How old is your second born?");
-var age5 = prompt("How old is your third born?");
-var age6 = prompt("How old is your fourth born?");
-var age7 = prompt("How old is your fifth born? God bless you, by the way!");
+
+var a;
+
+
+
+age1 = prompt("How old are you?");
+age2 = prompt("How old is your wife/Girlfriend/Husband/Boyfriend?");
+age3 = prompt("How old is your first born?");
+age4 = prompt("How old is your second born?");
+age5 = prompt("How old is your third born?");
+age6 = prompt("How old is your fourth born?");
+age7 = prompt("How old is your fifth born? God bless you, by the way!");
 
 
 var totalAge = function(){
-    var allAge = (age1 + age2 + age3 + age4 + age5 + age6 + age7);
-    return allAge;
+    a = (age1+age2+age3+age4+age5+age6+age7);
+    return a;
 
 };
+totalAge();
 
-age = totalAge;
 
-alert("Your family's total age is " + age);
+alert("Your family's total age is " + a);
 
-/*var calcArea = function(width, height){
-
-    var area = width * height;
-    return area;
-}
-
-var a = calcArea(20, 20);
-console.log (a);
