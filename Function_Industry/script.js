1285d41f5dac6a4028966714f3b8ccbfa4c35782
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-08-01 13:32:58 -0400

    starting code

--- Function_Industry/script.js
+++ Function_Industry/script.js
@@ -1,4 +1,11 @@
 Connor_Healy_Functions_Indusrty
 
 
+//Ths calculator will tell me how much under the amount of income I need daily to run my auto repair shop that I am each day.
+
+
+var income;
+var baseLine = 1000;
+
+function
 
