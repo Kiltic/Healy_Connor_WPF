8916dd4dbe5c0d8005ce72c11e384115fa16dc65
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-08-01 12:58:30 -0400

    Ok, couldn't figure out how to do the first one.  So I change it completelty. too bad.

--- Functions_Personal/script.js
+++ Functions_Personal/script.js
@@ -5,30 +5,25 @@
 
 
 
-alert("This calculator will add up the ages of everyone in your immediate family. " +
-    "Enter the ages below. If it doesn't apply leave it blank. 'Click OK when ready.'");
+alert("this calculator will tell you how many years you have left to live it the life expectancy is 80 years old.")
+
+yourAge = prompt("How old are you?");
+
+var yourAge;
+var expectAge = 80;
+var yearsLeft;
+
+function calcYears(){
+
+    yearsLeft = (expectAge - yourAge);
+    return yearsLeft;
+
+}
+
+calcYears();
 
 
-var a;
 
 
-
-age1 = prompt("How old are you?");
-age2 = prompt("How old is your wife/Girlfriend/Husband/Boyfriend?");
-age3 = prompt("How old is your first born?");
-age4 = prompt("How old is your second born?");
-age5 = prompt("How old is your third born?");
-age6 = prompt("How old is your fourth born?");
-age7 = prompt("How old is your fifth born? God bless you, by the way!");
-
-
-var totalAge = function(){
-    a = (age1+age2+age3+age4+age5+age6+age7);
-    return a;
-
-};
-totalAge();
-
-
-alert("Your family's total age is " + a);
+console.log(yearsLeft);
 
