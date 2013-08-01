9defa979afc50f389acd743f7410a63f3f0c9244
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-07-31 22:32:17 -0400

    practicing

--- Functions_Personal/script.js
+++ Functions_Personal/script.js
@@ -1,4 +1,20 @@
 //Connor_Healy_Functions_Personal
 
+var total = calcArea(20, 30);
 
+function calcArea(w, h){
+    var area = w * h;
+    return area;
 
+}
+
+console.log(total);
+
+var calcArea = function(width, height){
+
+    var area = width * height;
+    return area;
+}
+
+var a = calcArea(20, 20);
+console.log (a);
