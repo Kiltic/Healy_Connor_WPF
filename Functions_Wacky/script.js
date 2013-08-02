f2cdcac6c453ca228b3b775dc677adfba72bad75
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-08-01 21:48:27 -0400

    would have been a great idea.  I am too stupid to make it work

--- Functions_Wacky/script.js
+++ Functions_Wacky/script.js
@@ -1,6 +1,6 @@
 //Connor_Healy_Functions_Wacky
 
-//this will ask the user to type in 1-5. Depending on what number they type in it will give them a quote by the late comedian Mitch Hedberg.
+
 
 
 
