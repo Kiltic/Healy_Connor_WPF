4039d28fff97c1749a4ad5b37595407a3c672306
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-08-01 20:56:10 -0400

    test idea

--- Functions_Wacky/script.js
+++ Functions_Wacky/script.js
@@ -1,1 +1,17 @@
 Connor_Healy_Functions_Wacky
+
+//this will ask the user to type in 1-5. Depending on what number they type in it will give them a quote by the late comedian Mitch Hedberg.
+
+
+var joke;
+
+joke = prompt("enter a number between 1 and 5.");
+
+function jokes(){
+    if(joke === 1){
+        alert("I don't have a microwave but I do have a clock that occasionally cooks stuff!");
+    }
+}
+
+jokes();
+
