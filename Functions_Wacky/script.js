c8d5e49225b706c9c0832a1a178a20da8bab31ca
Connor Healy <Connor@Connors-MacBook-Pro.local>
2013-08-01 21:19:41 -0400

    trying again

--- Functions_Wacky/script.js
+++ Functions_Wacky/script.js
@@ -4,21 +4,32 @@
 
 
 var joke;
+var joke1;
+var joke2;
+/*var joke3;
+var joke4;
+var joke5;*/
+
+
 
 joke = prompt("enter a number between 1 and 5.");
 
 function jokes(){
-    if(joke === 1){
+    if(joke == 1){
         joke1 = ("I don't have a microwave but I do have a clock that occasionally cooks stuff!");
         return joke1;
+    }
+    if(joke == 2){
+        joke2 = ("I'm sick of following my dreams. I'm just gonna ask them where they are going and hook up with 'em later.");
+        return joke2;
     }
 }
 
 
 jokes();
+x = (joke1, joke2);
 
-var joke1;
 
-var x = joke1;
+
 
 alert(x);
